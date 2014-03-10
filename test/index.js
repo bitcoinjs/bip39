098ff3e4f70cc7f2b9b7e726aed85435fdc68091
Wei Lu <luwei.here@gmail.com>
2014-03-10 10:57:46 +0800

    one vector per test

--- test/index.js
+++ test/index.js
@@ -3,8 +3,8 @@ var bip39 = require('../index.js')
 var assert = require('assert')
 
 describe('mnemonicToSeed', function(){
-  it('works for tests vectors', function(){
-    vectors.forEach(function(v){
+  vectors.forEach(function(v, i){
+    it('works for tests vector ' + i, function(){
       assert.equal(bip39.mnemonicToSeed(v[1], 'TREZOR'), v[2])
     })
   })
