2c962e773571e9f22582b7a5328440bb8dcd951b
Henrik Hjelte <henrik@henrikhjelte.com>
2014-12-09 20:23:15 +0100

    add testcases mimicing the examples in README

--- test/index.js
+++ test/index.js
@@ -134,4 +134,38 @@ describe('BIP39', function() {
       })
     })
   })
+
+  describe('Examples in readme', function() {
+    var bip39 = BIP39
+
+    var mnemonic = bip39.entropyToMnemonic('133755ff') // hex input, defaults to BIP39 English word list
+    // 'basket rival lemon'
+    assert.ok((/^\w+ \w+ \w+$/).test(mnemonic))
+
+    var temp = bip39.mnemonicToEntropy(mnemonic) // hex input, defaults to BIP39 English word list
+    // '133755ff'
+    assert.equal(temp, '133755ff')
+
+    // Generate a random mnemonic using crypto.randomBytes
+    mnemonic = bip39.generateMnemonic() // strength defaults to 128 bits
+    //'bench maximum balance appear cousin negative muscle inform enjoy chief vocal hello'
+    assert.ok(/^(\w+ ){11}\w+$/.test(mnemonic))
+
+    var str = bip39.mnemonicToSeedHex('basket actual')
+    //'5cf2d4a8b0355e90295bdfc565a022a409af063d5365bb57bf74d9528f494bfa4400f53d8349b80fdae44082d7f9541e1dba2b003bcfec9d0d53781ca676651f'
+    assert.equal(str, '5cf2d4a8b0355e90295bdfc565a022a409af063d5365bb57bf74d9528f494bfa4400f53d8349b80fdae44082d7f9541e1dba2b003bcfec9d0d53781ca676651f')
+
+    var buff = bip39.mnemonicToSeed('basket actual')
+    var fiveC = 5*16+12
+    assert.equal(buff[0], fiveC)
+    // <Buffer 5c f2 d4 a8 b0 35 5e 90 29 5b df c5 65 a0 22 a4 09 af 06 3d 53 65 bb 57 bf 74 d9 52 8f 49 4b fa 44 00 f5 3d 83 49 b8 0f da e4 40 82 d7 f9 54 1e 1d ba 2b ...>
+
+    var bool = bip39.validateMnemonic(mnemonic)
+    // true
+    assert.ok(bool)
+
+    bool = bip39.validateMnemonic('basket actual')
+    // false
+    assert.ok(! bool)
+  })
 })
