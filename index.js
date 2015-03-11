f6f2c3779ffce38eea1b565530141371da48652b
Fynn <enriquefynn@gmail.com>
2015-03-11 02:13:29 -0300

    default to pt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var crypto = require('crypto')
 var pbkdf2 = require('pbkdf2-compat').pbkdf2Sync
 var unorm = require('unorm')
 
-var DEFAULT_WORDLIST = require('./wordlists/en.json')
+var DEFAULT_WORDLIST = require('./wordlists/pt.json')
 
 function mnemonicToSeed(mnemonic, password) {
   return pbkdf2(mnemonic, salt(password), 2048, 64, 'sha512')
