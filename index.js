c53b825116ae21224c7faf83256412c56de3ca83
Daniel Cousens <github@dcousens.com>
2016-10-06 00:08:39 +1100

    standard: use === over ==

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function entropyToMnemonic (entropy, wordlist) {
     return wordlist[index]
   })
 
-  return wordlist == JAPANESE_WORDLIST ? words.join('\u3000') : words.join(' ')
+  return wordlist === JAPANESE_WORDLIST ? words.join('\u3000') : words.join(' ')
 }
 
 function generateMnemonic (strength, rng, wordlist) {
