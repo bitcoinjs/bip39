12ec50c0bc6c0a4b9ed65a142f4fc5a16aff5f66
Kirill Fomichev <fanatid@ya.ru>
2017-03-14 16:10:53 +0300

    Rename wordlist

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var randomBytes = require('randombytes')
 // use unorm until String.prototype.normalize gets better browser support
 var unorm = require('unorm')
 
-var DEFAULT_WORDLIST = require('./wordlists/en.json')
-var JAPANESE_WORDLIST = require('./wordlists/ja.json')
+var ENGLISH_WORDLIST = require('./wordlists/english.json')
+var JAPANESE_WORDLIST = require('./wordlists/japanese.json')
+var DEFAULT_WORDLIST = ENGLISH_WORDLIST
 
 function salt (password) {
   return 'mnemonic' + (password || '')
@@ -133,7 +134,10 @@ module.exports = {
   generateMnemonic: generateMnemonic,
   validateMnemonic: validateMnemonic,
   wordlists: {
-    EN: DEFAULT_WORDLIST,
-    JA: JAPANESE_WORDLIST
+    EN: ENGLISH_WORDLIST,
+    JA: JAPANESE_WORDLIST,
+
+    english: ENGLISH_WORDLIST,
+    japanese: JAPANESE_WORDLIST
   }
 }
