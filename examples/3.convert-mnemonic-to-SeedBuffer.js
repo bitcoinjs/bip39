5118030e2f50953ec01553d49671c10d504abb4f
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-05-01 21:55:44 +0530

    fix standard test errors for 3.convert-mnemonic-to-SeedBuffer
    Log:
    /Users/prahaladbelavadi/Documents/grey/trial/BIP 39/bip39/examples/3.convert-mnemonic-to-SeedBuffer.js:1:1: 'bip39' is not defined.
    /Users/prahaladbelavadi/Documents/grey/trial/BIP 39/bip39/examples/3.convert-mnemonic-to-SeedBuffer.js:1:17: Strings must use singlequote.
    /Users/prahaladbelavadi/Documents/grey/trial/BIP 39/bip39/examples/3.convert-mnemonic-to-SeedBuffer.js:4:18: 'bip39' is not defined.
    /Users/prahaladbelavadi/Documents/grey/trial/BIP 39/bip39/examples/3.convert-mnemonic-to-SeedBuffer.js:6:24: Extra semicolon.

--- examples/3.convert-mnemonic-to-SeedBuffer.js
+++ examples/3.convert-mnemonic-to-SeedBuffer.js
@@ -1,6 +1,6 @@
-bip39 = require("../")
+var bip39 = require('../')
 
 var mnemonic = 'legend trust army surround organ clinic man impose arrow language island can'
 var seedBuffer = bip39.mnemonicToSeed(mnemonic)
 
-console.log(seedBuffer);
+console.log(seedBuffer)
