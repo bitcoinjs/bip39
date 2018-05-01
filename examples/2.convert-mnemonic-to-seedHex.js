258f12f481ce4fcf45137c562fe815d41fb28fed
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-05-01 21:54:18 +0530

    clarify different seed generations

--- examples/2.convert-mnemonic-to-seedHex.js
+++ examples/2.convert-mnemonic-to-seedHex.js
@@ -8,17 +8,17 @@ console.log('Deterministic Mnemonic: ' + mnemonic + '\n' + 'Seedhex: ', seedHex,
 // generate mnemonic and its corresponding seedhex ``
 mnemonic = bip39.generateMnemonic()
 seedHex = bip39.mnemonicToSeedHex(mnemonic)
-console.log('Generated Mnemonic: ' + mnemonic + '\n' + 'Generated Mnemonic\'s SeedHex: ' + seedHex)
+console.log('Generated Mnemonic: ' + mnemonic + '\n' + 'Generated Mnemonic\'s SeedHex: ' + seedHex, '\n')
 
 // If Mnemonic is passed as arguement, provides seedHex else generates one and logs it
 function genSeedhex (mnemonic) {
   if (mnemonic != null) {
     var seedHex = bip39.mnemonicToSeedHex(mnemonic)
-    console.log('Mnemonic: ' + mnemonic + '\n' + 'Seedhex: ', seedHex, '\n')
+    console.log('Given Mnemonic: ' + mnemonic + '\n' + 'Seedhex: ', seedHex, '\n')
   } else {
     mnemonic = bip39.generateMnemonic()
     seedHex = bip39.mnemonicToSeedHex(mnemonic)
-    console.log('Generated Mnemonic: ' + mnemonic + '\n' + 'Generated Mnemonic\'s SeedHex: ' + seedHex)
+    console.log('Generated Mnemonic: ' + mnemonic + '\n' + 'Generated Mnemonic\'s SeedHex: ' + seedHex, '\n')
   }
 }
 
