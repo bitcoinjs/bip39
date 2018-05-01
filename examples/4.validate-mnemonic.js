1ae42fb183ab6b34f8925da21132f86dcf928f5a
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-05-01 22:03:01 +0530

    fix standard test errors for 4.validateMnemonic.js example
    log:lost terminal history but it works!

--- examples/4.validate-mnemonic.js
+++ examples/4.validate-mnemonic.js
@@ -1,14 +1,16 @@
 // Checks if mnemonic is valid(provides >128 bits of entropy)
 
-bip39 = require("../")
+var bip39 = require('../')
 
 var mnemonic = 'legend trust army surround organ clinic man impose arrow language island can'
 
 // true implies it is valid and false imples that mnemonic is invalid
-function val(mnemonic){
-  if (bip39.validateMnemonic(mnemonic)==true){
-    return "valid"
-  }else{return "invalid"}
+function val (mnemonic) {
+  if (bip39.validateMnemonic(mnemonic) === true) {
+    return 'valid'
+  } else {
+    return 'invalid'
+  }
 }
 
-console.log("The Mnemonic '"+mnemonic+"' is " + val(mnemonic));
+console.log('The provided Mnemonic: \'' + mnemonic + '\' is ' + val(mnemonic))
