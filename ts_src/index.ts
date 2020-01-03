f56d0f6a4cdab418fece4f376665d70e6367ad90
yvette patrick <yvettepatrick@renee.attlocal.net>
2020-01-03 15:32:38 -0800

    Revert "Merge pull request #120 from Mr-jiangzhiguo/master"
    This reverts commit 372555c5f6d170d5015c8a9a965bb2698ae1c7be, reversing
    changes made to e18e1e4e8fe6e9a42c04b478bbcf810facd44b4a.

--- ts_src/index.ts
+++ ts_src/index.ts
@@ -36,7 +36,7 @@ function deriveChecksumBits(entropyBuffer: Buffer): string {
     .update(entropyBuffer)
     .digest();
 
-  return bytesToBinary(Array.from(hash)).slice(0, CS);
+  return bytesToBinary([...hash]).slice(0, CS);
 }
 
 function salt(password?: string): string {
@@ -128,7 +128,7 @@ export function entropyToMnemonic(
   if (entropy.length > 32) throw new TypeError(INVALID_ENTROPY);
   if (entropy.length % 4 !== 0) throw new TypeError(INVALID_ENTROPY);
 
-  const entropyBits = bytesToBinary(Array.from(entropy));
+  const entropyBits = bytesToBinary([...entropy]);
   const checksumBits = deriveChecksumBits(entropy);
 
   const bits = entropyBits + checksumBits;
